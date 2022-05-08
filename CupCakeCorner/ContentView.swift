f490b9e7dc31b27c822068e8ffc15b2e1a9851b8
Gokhnan Bozkurt <gkhnbzkrt1000@gmail.com>
2022-05-08 10:41:12 +0300

    Initial Commit

---
+++ CupCakeCorner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CupCakeCorner
+//
+//  Created by Gokhan Bozkurt on 8.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CupCakeCorner/CupCakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCakeCornerApp.swift
+//  CupCakeCorner
+//
+//  Created by Gokhan Bozkurt on 8.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct CupCakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
